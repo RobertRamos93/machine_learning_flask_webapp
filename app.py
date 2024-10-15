26691d05593f6ab8dd8beb233e2bafe70fb2e155
Roberto Ramos <100817696+RobertRamos93@users.noreply.github.com>
2024-10-14 23:02:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-# Dependencias
-from flask import Flask, request
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
 import joblib
 import pandas as pd
 
-# Crear instancia de Flask y Api
 app = Flask(__name__)
 api = Api(app)
 
@@ -15,32 +13,46 @@ class Predict(Resource):
     def post(self):
         # Captura de los datos
         data = request.get_json()
+        
+        age = int(data['age'])
+        sex = data['sex']
+        cp = data['cp']
+        trestbps = int(data['trestbps'])
+        chol = int(data['chol'])
+        fbs = int(data['fbs'])
+        restecg = int(data['restecg'])
+        thalach = int(data['thalach'])
+        exang = int(data['exang'])
+        oldpeak = float(data['oldpeak'])
+        slope = int(data['slope'])
+        ca = int(data['ca'])
+        thal = int(data['thal'])
 
-        # Crear DataFrame con las características
+        # DataFrame con las características
         df = pd.DataFrame({
-            'age': [int(data['age'])],
-            'trestbps': [int(data['trestbps'])],
-            'chol': [int(data['chol'])],
-            'fbs': [int(data['fbs'])],
-            'restecg': [int(data['restecg'])],
-            'thalach': [int(data['thalach'])],
-            'exang': [int(data['exang'])],
-            'oldpeak': [float(data['oldpeak'])],
-            'slope': [int(data['slope'])],
-            'ca': [int(data['ca'])],
-            'thal': [int(data['thal'])],
-            'sex_male': [1 if data['sex'] == 'male' else 0],
-            'cp_atypical angina': [1 if data['cp'] == 'atypical angina' else 0],
-            'cp_non-anginal pain': [1 if data['cp'] == 'non-anginal pain' else 0],
-            'cp_typical angina': [1 if data['cp'] == 'typical angina' else 0]
+            'age': [age],
+            'trestbps': [trestbps],
+            'chol': [chol],
+            'fbs': [fbs],
+            'restecg': [restecg],
+            'thalach': [thalach],
+            'exang': [exang],
+            'oldpeak': [oldpeak],
+            'slope': [slope],
+            'ca': [ca],
+            'thal': [thal],
+            'sex_male': [1 if sex == 'male' else 0],
+            'cp_atypical angina': [1 if cp == 'atypical angina' else 0],
+            'cp_non-anginal pain': [1 if cp == 'non-anginal pain' else 0],
+            'cp_typical angina': [1 if cp == 'typical angina' else 0]
         })
 
         # Predicción
         prediction = model.predict(df)
         result = "No Heart disease" if prediction[0] == 1 else "Heart disease"
-        return {'result': result}
+        return jsonify({'result': result})
 
-# Agregar la ruta de la API
+# Configuración de las rutas
 api.add_resource(Predict, '/predict')
 
 if __name__ == '__main__':
